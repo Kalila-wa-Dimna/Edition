b5d5031cb6c2b54a3c8e28899b3bf80ac63ae32e
marwaahmed <2101-marwaahmed@users.noreply.git.imp.fu-berlin.de>
2023-11-15 12:51:27 +0100

    illustration Gallery has been added

---
+++ libs/section-manuscripts/src/lib/illustrations-gallery/illustration-modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA,  MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-illustration-modal',
+  template: `
+    <img [src]="data.illustration" alt="Illustration">
+    <button mat-button (click)="closeDialog()">Close</button>
+  `,
+})
+export class IllustrationModalComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { illustration: string },
+    private dialogRef: MatDialogRef<IllustrationModalComponent>
+  ) {}
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
+}
