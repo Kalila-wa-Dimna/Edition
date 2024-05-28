10f40dc4bbe82c5879e6984fc52b8a5b940ae71b
mahmoudkozae <mahmoud.kozae@fu-berlin.de>
2024-05-28 16:13:44 +0200

    update to nx@19 and angular@18

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { APP_ID, NgModule, isDevMode,  NO_ERRORS_SCHEMA } from '@angular/core';
+import { APP_ID, NgModule, isDevMode, NO_ERRORS_SCHEMA } from '@angular/core';
 import { BrowserModule, provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -14,7 +14,7 @@ import {
 } from '@kalila-edition/common-ui';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { MainPageComponent } from './main-page.component';
-import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
 import { AboutPageComponent } from './about-page.component';
 import { ImprintPageComponent } from './imprint-page.component';
 import { environment } from './environment';
@@ -26,12 +26,13 @@ import { environment } from './environment';
     AboutPageComponent,
     ImprintPageComponent,
   ],
-  imports: [
-    BrowserModule,
+  schemas: [
+    NO_ERRORS_SCHEMA
+  ],
+  bootstrap: [AppComponent], imports: [BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     ConfigModule.forRoot(environment),
     BrowserAnimationsModule,
-    HttpClientModule,
     StorageModule,
     ThemeModule,
     LayoutModule,
@@ -42,18 +43,10 @@ import { environment } from './environment';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000',
-    })
-  ], schemas: [
-    NO_ERRORS_SCHEMA
-  ],
-  providers: [{ provide: APP_ID, useValue: 'kd-edition' }, provideClientHydration(withHttpTransferCacheOptions({
-    includePostRequests: true
-  })), [
-    provideHttpClient(
-      withFetch(),
-    ),
-  ]],
-  bootstrap: [AppComponent],
-
+    })], providers: [{ provide: APP_ID, useValue: 'kd-edition' }, provideClientHydration(withHttpTransferCacheOptions({
+      includePostRequests: true
+    })), [
+      provideHttpClient(withFetch()),
+    ], provideHttpClient(withInterceptorsFromDi())]
 })
 export class AppModule { }
