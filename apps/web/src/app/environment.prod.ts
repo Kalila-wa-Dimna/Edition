9977323ac8d1105fbb868f373b7796784e4feef4
mahmoudkozae <mahmoud.kozae@fu-berlin.de>
2024-06-03 17:26:34 +0200

    fixed version

--- apps/web/src/app/environment.prod.ts
+++ apps/web/src/app/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment: IConfig = {
   imagesEndPoint: 'https://d2u9osbhl165ia.cloudfront.net/srv/data/edition_data/images/',
   dataEndPoint: 'https://d2u9osbhl165ia.cloudfront.net/srv/data/edition_data/',
   pagesEndPoint: 'https://d2u9osbhl165ia.cloudfront.net/srv/page/',
-  version: '0.5.1',
+  version: '0.5.3',
 };
