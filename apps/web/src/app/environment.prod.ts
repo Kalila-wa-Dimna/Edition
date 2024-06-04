5bf6e05f6fe484fad74f9e668271c9bf882e0b39
mahmoudkozae <mahmoud.kozae@fu-berlin.de>
2024-06-04 13:06:33 +0200

    made a sepaearte file for version

--- apps/web/src/app/environment.prod.ts
+++ apps/web/src/app/environment.prod.ts
@@ -1,8 +1,9 @@
 import { IConfig } from '@kalila-edition/common-ui';
+import version from './version';
 
 export const environment: IConfig = {
   imagesEndPoint: 'https://d2u9osbhl165ia.cloudfront.net/srv/data/edition_data/images/',
   dataEndPoint: 'https://d2u9osbhl165ia.cloudfront.net/srv/data/edition_data/',
   pagesEndPoint: 'https://d2u9osbhl165ia.cloudfront.net/srv/page/',
-  version: '0.5.3',
+  version,
 };
