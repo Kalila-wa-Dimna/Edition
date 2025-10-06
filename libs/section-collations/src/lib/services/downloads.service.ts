241d6358c0c5b3c6cd358a27ba922e060b9d9111
marwaahmed <2101-marwaahmed@users.noreply.git.imp.fu-berlin.de>
2025-10-07 00:41:49 +0200

    download Edition Table has been added

--- libs/section-collations/src/lib/services/downloads.service.ts
+++ libs/section-collations/src/lib/services/downloads.service.ts
@@ -9,14 +9,11 @@ import { firstValueFrom } from 'rxjs';
 })
 export class DownloadsService {
 
-
-
   constructor(private httpClient: HttpClient,
     @Inject(CONFIG_TOKEN) private config: IConfig) { }
 
-
   collationName: string | null = null;
-  unitTableEndpoint = (collationName: string) => `${this.config.dataEndPoint}collations/${collationName}/unit_table.csv`
+  unitTableEndpoint = (collationName: string) => `${this.config.dataEndPoint}collations/${collationName}`
 
   init(collationName: string) {
     this.collationName = collationName;
@@ -29,7 +26,7 @@ export class DownloadsService {
 
     const endpoint = this.unitTableEndpoint(this.collationName);
     try {
-      const data = await firstValueFrom(this.httpClient.get(endpoint, { responseType: 'text' }));
+      const data = await firstValueFrom(this.httpClient.get(endpoint+'/unit_table.csv', { responseType: 'text' }));
       const blob = new Blob([data], { type: 'text/csv' });
       const url = window.URL.createObjectURL(blob);
       const a = document.createElement('a');
@@ -44,4 +41,30 @@ export class DownloadsService {
     }
   }
 
+
+
+
+async downloadEditionTable() {
+  if (!this.collationName) {
+    throw new Error('Collation name is not set.');
+  }
+
+  const endpoint = this.unitTableEndpoint(this.collationName);
+  try {
+    const data = await firstValueFrom(this.httpClient.get(endpoint+'/segment_tokens.csv', { responseType: 'text' }));
+    const blob = new Blob([data], { type: 'text/csv' });
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${this.collationName}_edition.csv`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  } catch (error) {
+    console.error('Error downloading unit table:', error);
+  }
 }
+
+}
+
